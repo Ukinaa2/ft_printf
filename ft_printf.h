8541bdc5858a96c1f361106fd34027276cf81b1e
gguedes <gguedes@student.42.rio>
2022-06-10 14:00:21 -0300

    Makefile changes

--- ft_printf.h
+++ ft_printf.h
@@ -6,14 +6,14 @@
 /*   By: gguedes <gguedes@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 13:38:14 by gguedes           #+#    #+#             */
-/*   Updated: 2022/06/09 13:36:59 by gguedes          ###   ########.fr       */
+/*   Updated: 2022/06/10 13:42:42 by gguedes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include "./libft/libft.h"
+# include "libft/libft.h"
 # include <stdarg.h>
 
 int	ft_printf(const char *str, ...);
