4d15f0e6b35ebf96833ecff07c164fcc8ed224f8
gguedes <gguedes@student.42.rio>
2022-06-10 18:57:38 -0300

    all tests ok, not norm check

--- libft/ft_putunbr_fd.c
+++ libft/ft_putunbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: gguedes <gguedes@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 12:59:08 by gguedes           #+#    #+#             */
-/*   Updated: 2022/06/10 13:03:41 by gguedes          ###   ########.fr       */
+/*   Updated: 2022/06/10 18:53:12 by gguedes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ int	ft_putunbr_fd(unsigned int n, int fd)
 	i = 0;
 	if (n > 9)
 		i += ft_putnbr_fd(n / 10, fd);
-	if (n >= 0)
-		i += ft_putchar_fd((n % 10) + '0', fd);
+	i += ft_putchar_fd((n % 10) + '0', fd);
 	return (i);
 }
